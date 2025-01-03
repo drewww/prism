98f41f7b252ee1285b0df726fa5cc35aa97c1982
Matthew Blanchard <matthewrblanchard@gmail.com>
2025-01-03 08:29:42 -0500

    Fixed rect tool.

--- geometer/tools/rect.lua
+++ geometer/tools/rect.lua
@@ -15,15 +15,16 @@ end
 ---@param x integer The cell coordinate clicked.
 ---@param y integer The cell coordinate clicked.
 function RectTool:mouseclicked(geometer, level, x, y)
-   if x < 0 or x > level.map.w then return end
-   if y < 0 or y > level.map.h then return end
+   if x < 1 or x > level.map.w then return end
+   if y < 1 or y > level.map.h then return end
 
    self.topleft = prism.Vector2(x, y)
 end
 
 function RectTool:mousereleased(geometer, level, x, y)
-   local x = math.min(level.map.w, math.max(0, x))
-   local y = math.min(level.map.h, math.max(0, y))
+   if not self.topleft then return nil end
+   local x = math.min(level.map.w, math.max(1, x))
+   local y = math.min(level.map.h, math.max(1, y))
 
    local lx, ly, rx, ry = self:getCurrentRect(x, y)
    local modification = RectModification(prism.cells.Wall, prism.Vector2(lx, ly), prism.Vector2(rx, ry))
