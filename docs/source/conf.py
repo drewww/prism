71d455975d84613fb9a075dee1a755ad5517564c
LJNIC <liamreadsspam@gmail.com>
2025-04-10 21:24:38 -0300

    Janky styles

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,11 @@ exclude_patterns = []
 
 root_doc = 'index'
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+html_css_files = [
+    'terminal.css'
+]
