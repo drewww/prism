c913c396a16852cfcfac08d72621b7591ed9c9f3
Matthew Blanchard <matthewrblanchard@gmail.com>
2025-07-13 13:17:53 -0400

    More doc comments for Position.

--- engine/core/components/position.lua
+++ engine/core/components/position.lua
@@ -1,4 +1,6 @@
---- Defines a position for the actor.
+--- Defines a position for the actor. You should avoid
+--- directly manipulating this and instead use level:moveActor
+--- and actor:getPosition or actor:expectPosition.
 --- @class Position : Component
 --- @field private _position Vector2
 --- @overload fun(pos: Vector2?): Position
@@ -8,6 +10,8 @@ function Position:__new(pos)
    self._position = pos or prism.Vector2(1, 1)
 end
 
+--- Gets a reference to the inner vector. Don't pass this around
+--- or modify this! Treat it as immutable!
 function Position:getVector()
     return self._position
 end
