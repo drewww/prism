fed500d6a91e1e630701b46c8189c3581f653577
Matthew Blanchard <matthewrblanchard@gmail.com>
2025-07-22 22:38:10 -0400

    Inventory fixes, fixed item:split checking count > 1 rather than count >= 1

--- extra/inventory/module.lua
+++ extra/inventory/module.lua
@@ -2,5 +2,5 @@ local path = ...
 -- Strip the last component of the path
 local basePath = path:match("^(.*)%.") or ""
 
---- @module "Inventory.inventorytarget"
+--- @module "extra.inventory.inventorytarget"
 prism.InventoryTarget = require(basePath .. ".inventorytarget")
