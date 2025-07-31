fcc49e530f4f9a6804f2b52442edfbdd8326bc66
LJNIC <liamreadsspam@gmail.com>
2025-07-31 08:44:29 -0300

    Dynamic registries

---
+++ engine/meta.lua
@@ -0,0 +1,23 @@
+--- The actor registry.
+prism.actors = {}
+
+--- The actions registry.
+prism.actions = {}
+
+--- The component registry.
+prism.components = {}
+
+--- The component registry.
+prism.cells = {}
+
+--- The target registry.
+prism.targets = {}
+
+--- The message registry.
+prism.messages = {}
+
+--- The system registry.
+prism.systems = {}
+
+--- The decision registry.
+prism.decisions = {}
