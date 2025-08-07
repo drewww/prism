3102882d8f6e8a870e7598c1c92c1251329bccaf
Matthew Blanchard <matthewrblanchard@gmail.com>
2025-08-07 11:38:16 -0400

    Reverted the reverted classname on senses, again.

--- engine/core/relationships/senses.lua
+++ engine/core/relationships/senses.lua
@@ -2,7 +2,7 @@
 --- This is the inverse of the `SensedBy` relationship.
 --- @class Senses : Relationship
 --- @overload fun(): Senses
-local Senses = prism.Relationship:extend "Senses"
+local Senses = prism.Relationship:extend "SensesRelationship"
 
 --- @return Relationship sensedby inverse `SensedBy` relationship.
 function Senses:generateInverse()
