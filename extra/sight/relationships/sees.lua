1af5047ce0c0e6d42d07ee5721dedd618ee5f608
Matthew Blanchard <matthewrblanchard@gmail.com>
2025-08-07 11:17:37 -0400

    Updated classname and moved file.

--- extra/sight/relationships/sees.lua
+++ extra/sight/relationships/sees.lua
@@ -1,6 +1,6 @@
 --- @class Sees : Relationship
 --- @overload fun(): Sees
-local Sees = prism.Relationship:extend "Seen"
+local Sees = prism.Relationship:extend "Sees"
 
 function Sees:generateInverse()
    return prism.relationships.SeenBy
